87a4a11419939306b40fa43cbd29660594b6dd98
VirajRG <viraj.gawde123@gmail.com>
2020-04-11 15:39:23 +0530

    added antd design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import './App.css';
-import v1 from "./videos/v1.mp4"
 import Test from "./Test"
+import { DatePicker } from 'antd';
+
+import 'antd/dist/antd.css';
+
 function App() {
-  // const [data, setData] = useState([])
-  // useEffect(()=> {
-  //   const fetchData = async () => {
-  //     const result = await fetch('http://orangevalleycaa.org/api/videos').then(
-  //       response => response.json()
-  //     )
-  //     setData(result);
-  //   }
-  //   fetchData();
-  // }, []);
   return (
     <div className="App">
       <header>
         <h1>Videos</h1>
       </header>
+      <DatePicker />
       <Test />
     </div>
   );
