ffe0c72bc7560e61985b868036fd7b53324587bf
VirajRG <viraj.gawde123@gmail.com>
2020-04-16 22:49:19 +0530

    login binding done

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { Button } from "antd"
+import axios from 'axios'
 
 import history from '../../history';
 
@@ -12,15 +13,40 @@ class Login extends React.Component {
         this.props.setUser("viraj");
     }
     componentDidMount() {
-        fetch("http://localhost:8080")
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    console.log("result : ", result)
-                },
-                (error) => {
-                }
-            )
+        axios
+            .post('https://prism-organized-column.glitch.me/login/', {username:'viraj', pass: '123'})
+            .then((res) => console.log(res.data))
+            .catch(err => {
+                console.error(err);
+            });
+        // fetch("https://prism-organized-column.glitch.me", {
+        //     method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        //     mode: 'cors', // no-cors, *cors, same-origin
+        //     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        //     credentials: 'same-origin', // include, *same-origin, omit
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     },
+        //     redirect: 'follow', // manual, *follow, error
+        //     referrerPolicy: 'no-referrer', // no-referrer, *client
+        //     body: JSON.stringify({ username: 'viraj', pass: 'v' }) // body data type must match "Content-Type" header
+        // }).then(res => res.json())
+        //     .then(
+        //         (result) => {
+        //             console.log("result : ", result)
+        //         },
+        //         (error) => {
+        //         }
+        //     );
+        // fetch("https://prism-organized-column.glitch.me")
+        //     .then(res => res.json())
+        //     .then(
+        //         (result) => {
+        //             console.log("result : ", result)
+        //         },
+        //         (error) => {
+        //         }
+        //     )
     }
     render() {
         return (
